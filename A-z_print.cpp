b3cd07ff2c36f2934fe0428dd3bed98ae9e86820
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-03 15:34:58 +0530

    print A to Z

---
+++ A-z_print.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(char i = 65 ; i < 91 ; i++){
+        cout<<i<<" ";
+    }
+}
--- StringReverse.cpp
+++ StringReverse.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 int main(){
     string name;
+    string rev_name;
     cout<<"enter the name\n"; //Sarvadnya
     getline(cin, name);
     int size = name.length();
     for(int i = size; i >= 0; i-- ){
-        cout<<name[i]; //ayndavras
+        rev_name[i]=name[size-i];
     }
+    cout<<rev_name;
 }
