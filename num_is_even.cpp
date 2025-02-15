88c95768febb58057e65ff354dafc8b6b2b78401
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-16 00:55:01 +0530

    is even or not

---
+++ num_is_even.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+bool iseven(int num){
+    if(num % 2 == 0)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+int main(){
+
+    cout<< iseven(1234242);
+
+}
+
+// output 
+// 1
