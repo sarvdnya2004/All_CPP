fdb3e507adc386ba0bd1a6fd268e5454f5b69414
Sarvadnya Mense <mensesarvadnya@gmail.com>
2025-02-19 22:56:45 +0530

    2nd approach for reversing array

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -20,3 +20,28 @@ int main(){
 }
 
 // 34 23 12 
+
+// approach 2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int rev(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+    while(start <= end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+int main(){
+
+    int arra[3] = {12,23,34};
+    rev(arra, 3);
+    for(int i : arra){
+        cout<<i<<" ";
+    }
+}
+
+// i think 2nd approach is the best 
